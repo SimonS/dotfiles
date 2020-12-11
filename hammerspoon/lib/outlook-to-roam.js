7478e80007671feefa63ea88a8d2cde9f8d13235
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-12-11 23:56:49 +0000

    tidy

---
+++ hammerspoon/lib/outlook-to-roam.js
@@ -0,0 +1,31 @@
+const timeToString = (time) =>
+  `${time
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${time.getMinutes().toString().padStart(2, "0")}`;
+
+Application("Microsoft Outlook")
+  .selectedObjects()
+  .map((ev) => ({
+    title: ev.subject(),
+    time: `${timeToString(ev.startTime())}`,
+    attendees: [
+      ...ev.attendees().map((att) => `[[${att.emailAddress().name}]]`),
+    ],
+    agenda: ev
+      .plainTextContent()
+      .trim()
+      .split(/[\r\n]+/)
+      .join(" ----- "),
+    location: ev.location(),
+  }))
+  .sort((a, b) => a.time - b.time)
+  .map(
+    (ev) => `[[Meeting/${ev.title}]] - ${ev.time}
+        - Category:: #meeting
+        - Attendees
+        - ${ev.attendees.join("\n    ")}
+        - Agenda\n
+        - ${ev.agenda}`
+  )
+  .join("\n");
